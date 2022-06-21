a699df0c8710d7c39a16f0825f05914bbb689777
EC2 Default User <ec2-user@ip-172-31-35-94.ap-northeast-1.compute.internal>
2022-06-21 12:38:55 +0000

    [Add]いいね機能の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
 
   validates :title,presence:true
   validates :body ,length:{minimum:1, maximum:200}
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 
 end
+  
